762e153a6228ba5dc5e70130983fd3439c6e81c5
William <wtpwinberg@gmail.com>
2018-12-28 17:13:45 -0800

    start from scratch

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
+// import thr react and react dom libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// var getButtonText = () => {
+//    return 'Click on me!';
+// }
+
+// create a react component
+const App = () => {
+   var buttonText = { yo: 'foo' };
+   var labelText = 'Enter name';
+
+   return (
+      <div>
+         <label className="label" htmlFor="name">
+            {labelText}
+         </label>
+         <input id="name" type="text" />
+         <button style={{
+            background: 'blue',
+            color: 'white',
+            border: '1px solid black',
+         }}>
+            {buttonText.yo}
+         </button>
+      </div>
+   );
+}
+
+   ;
+
+// show component on screen
+ReactDOM.render(
+   <App />, document.querySelector('#root')
+);
+
+//looks like HTML, its JSX
+// makes use of deoendency called babel
